6069fe2a0e18b6c18acd02268017451243247c6c
Eliezer556 <eliezerdma556@gmail.com>
2025-12-19 15:29:52 -0400

    urls fix production config

--- E-commerce/bakend/config/urls.py
+++ E-commerce/bakend/config/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve 
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,5 +12,12 @@ urlpatterns = [
     path('orders/', include('apps.orders.urls'))
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
+# Produccion: 
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
+]
