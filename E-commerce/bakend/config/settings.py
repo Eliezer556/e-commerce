8d1ebd0e341a5021f54ea54d071912363f9dfae5
Eliezer556 <eliezerdma556@gmail.com>
2025-12-19 15:05:35 -0400

    fixed CORS5 false

--- E-commerce/bakend/config/settings.py
+++ E-commerce/bakend/config/settings.py
@@ -156,7 +156,7 @@ CORS_ALLOWED_ORIGINS = [
     "https://e-commerce-tu-nombre.vercel.app",
 ]
 
-CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_CREDENTIALS = False
 
 CORS_ALLOW_HEADERS = [
     'accept',
