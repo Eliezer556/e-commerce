cdde959070f17ca7493ecba42b8b134498aa4e5f
Eliezer556 <eliezerdma556@gmail.com>
2025-12-19 14:45:59 -0400

    fix cords settings

--- E-commerce/bakend/config/settings.py
+++ E-commerce/bakend/config/settings.py
@@ -153,6 +153,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "https://e-commerce-shkc.onrender.com",
+    "https://e-commerce-tu-nombre.vercel.app/",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
